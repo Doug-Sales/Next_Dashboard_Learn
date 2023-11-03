70c0e445a40c4b0e2fcca5425a6672a6e3ccf0c4
Doug-Sales <douglasmsales28@outlook.com>
2023-11-03 20:23:05 -0300

    Add action server and ui

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -171,15 +171,18 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
+    
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
+    console.log(invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice.');
   }
 }
 
